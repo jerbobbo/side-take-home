e0465d5574b594607f11aa3ebf10aa9f044b3733
Rob Debbane <studio@robertdebbane.com>
2021-09-30 12:38:20 -0400

    feat: 🎸 added bearer authentication

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const { ApolloServer,  } = require('apollo-server-express');
+const bearerToken = require('express-bearer-token');
+const config = require('config');
+const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./types');
 const { SimplyRetsAPI } = require('./stores/simplyrets/simply-rets-api');
 const resolvers = require('./resolvers');
-const config = require('config');
 const { connectDb, getListingsCollection } = require('./stores/mongodb/store');
 const { Listings } = require('./stores/mongodb/Listings');
+const bearerAuth = require('./middleware/bearer-auth');
 
 (async () => {
     const isDevelopment = config.get('env') === 'development';
@@ -36,6 +38,8 @@ const { Listings } = require('./stores/mongodb/Listings');
         
         await server.start();
         app.use(morgan('combined'));
+        app.use(bearerToken());
+        app.use(bearerAuth);
         server.applyMiddleware({ app, path:"/graphql" });
     } catch (error) {
         console.error(error);
---
+++ middleware/bearer-auth.js
@@ -0,0 +1,14 @@
+const { AuthenticationError } = require("apollo-server-errors");
+const { getUsersCollection } = require("../stores/mongodb/store");
+
+module.exports = async function(req, res, next) {
+  const { token } = req;
+  const userCollection = getUsersCollection();
+  const foundUser = await userCollection.findOne({ token });
+  if (!foundUser) {
+    const err = new Error('Unauthorized');
+    err.status = 401;
+    return next(err);
+  }
+  next();
+}

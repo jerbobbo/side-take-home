445ad6b3cc865bb6d15abcf11b358b2676991665
Rob Debbane <studio@robertdebbane.com>
2021-09-29 23:08:29 -0400

    feat: 🎸 added favoriteCount resolver

--- resolvers/index.js
+++ resolvers/index.js
@@ -3,5 +3,10 @@ module.exports = {
     properties: async (parent, { city }, { dataSources }) => {
       return dataSources.simplyRetsApi.getListings(city);
     }
+  },
+  Listing: {
+    favoriteCount: async (parent, _, { dataSources }) => {
+      return dataSources.listings.getFavoriteCountById(parent.listingId);
+    }
   }
 };
